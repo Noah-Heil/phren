598567e7200c23234f12e22ddb9444ebe5766a86
Heil, Noah <Noah.Heil@ncr.com>
2018-05-28 13:59:51 -0400

    ok resetting changes

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Receptor stub
+type Receptor struct {
+}
+
+// Drug stub
+type Drug struct {
+}
+
+// Person stub
+type Person struct {
+}
+
+func main() {
+	fmt.Println("hello world")
+}
