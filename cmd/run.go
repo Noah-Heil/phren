e9cfbe99471e91f34d6b27b57f4b5e5f445af113
Heil, Noah <Noah.Heil@ncr.com>
2018-05-31 13:16:46 -0400

    fancy openfile methods defined utilizing logrus, color, and cobra packages

---
+++ cmd/run.go
@@ -0,0 +1,120 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/fatih/color"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		log.Debug("Entered Run cmd")
+		fmt.Println("What XML file would you like to read from?")
+		FileName := GetXMLFileName(".xml")
+		log.WithFields(log.Fields{"FileName": FileName, "Location": "main func past GetXMLFileName"}).Debug("FileName retrieved from user")
+
+		OpenXMLFile(FileName)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+
+	switch {
+	case strings.Compare(loglevel, "debug") == 0:
+		log.SetLevel(log.DebugLevel)
+	case strings.Compare(loglevel, "info") == 0:
+		log.SetLevel(log.InfoLevel)
+	case strings.Compare(loglevel, "error") == 0:
+		log.SetLevel(log.ErrorLevel)
+	case strings.Compare(loglevel, "fatal") == 0:
+		log.SetLevel(log.FatalLevel)
+	case strings.Compare(loglevel, "panic") == 0:
+		log.SetLevel(log.PanicLevel)
+	default:
+		log.SetLevel(log.WarnLevel)
+	}
+
+	switch {
+	case strings.Compare(setOutput, "stdout") == 0:
+		log.SetOutput(os.Stdout)
+	}
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// runCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// OpenXMLFile Opens and XML File
+func OpenXMLFile(FileName string) (xmlFile *os.File) {
+	log.WithFields(log.Fields{"FileName": FileName, "Location": "Begin OpenXMLFile"}).Debug("Entering OpenXMLFile")
+	xmlFile, err := os.Open(strings.TrimSpace(FileName)) // Open our xmlFile
+	if err != nil {
+		log.WithFields(log.Fields{"FileName": FileName, "Location": "open err check", "Error": err}).Error("Failed to open file")
+		fmt.Println(color.HiRedString("%+v\n", err))
+		return
+	}
+	fmt.Printf(color.GreenString("Successfully Opened %s\n", FileName))
+	defer xmlFile.Close() // defer the closing of our xmlFile so that we can parse it later on
+	return xmlFile
+}
+
+// IsPathExtensionNotValid just checks file extension type and if it is not valid then it rejects the file
+func IsPathExtensionNotValid(FileName string, ValidExtension string) bool {
+	log.WithFields(log.Fields{"FileName": FileName, "Location": "Inside IsPathExtensionNotValid func"}).Debug("Check FileName's validity")
+	if filepath.Ext(strings.TrimSpace(FileName)) != ValidExtension {
+		log.WithFields(log.Fields{"FileName": FileName, "Location": "Inside if statement IsPathExtensionNotValid func"}).Debug("FileName is not valid. Try again!")
+		fmt.Println(color.HiRedString("Error! file must be xml file"))
+		return true
+	}
+	log.WithFields(log.Fields{"FileName": FileName, "Location": "Past if statement IsPathExtensionNotValid func filename should be valid now"}).Debug("FileName is valid!")
+	return false
+}
+
+// GetXMLFileName gets the name of the xml file we want to read from
+func GetXMLFileName(ValidExtension string) string {
+	var FileName string
+	IsNotValid := true
+	for IsNotValid {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter text: ")
+		FileName, _ = reader.ReadString('\n')
+		log.WithFields(log.Fields{"FileName": FileName, "Location": "Inside for loop GetXMLFileName func"}).Debug("FileName retrieved from user")
+		IsNotValid = IsPathExtensionNotValid(FileName, ValidExtension)
+	}
+	log.WithFields(log.Fields{"FileName": FileName, "Location": "exiting GetXMLFileName func"}).Debug("FileName is valid; returning to main")
+	return FileName
+}
--- main.go
+++ main.go
@@ -1,174 +1,21 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-// // Person stub
-// type Person struct {
-// 	Drugs map[string]Drug
-// }
-
-// // Drug stub
-// type Drug struct {
-// 	TargetReceptors map[string]Receptor
-// }
-
-// // Receptor stub
-// type Receptor struct {
-// 	SideEffects map[string]SideEffect
-// }
-
-// // SideEffect stub
-// type SideEffect struct {
-// 	Description string
-// 	Count       int
-// }
-
-// var m map[string]int
-
-// // Joe stub
-// var Joe Person
-
-// // TmpDrug stub
-// var TmpDrug Drug
-
-// // TmpReceptor stub
-// var TmpReceptor Receptor
-
-// // TmpSideEffect stub
-// var TmpSideEffect SideEffect
-
-// func LoadSideEffect() {
-// 	list := arraylist.New()
-
-// }
-
-// func newClient() *dgo.Dgraph {
-// 	// Dial a gRPC connection. The address to dial to can be configured when
-// 	// setting up the dgraph cluster.
-// 	d, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	return dgo.NewDgraphClient(
-// 		api.NewDgraphClient(d),
-// 	)
-// }
-
-// func setup(c *dgo.Dgraph) {
-// 	// Install a schema into dgraph. Accounts have a `name` and a `balance`.
-// 	err := c.Alter(context.Background(), &api.Operation{
-// 		Schema: `
-// 			name: string @index(term) .
-// 			balance: int .
-// 		`,
-// 	})
-// }
-
-// func nuke_from_orbit(c *dgo.Dgraph) {
-// 	err := c.Alter(context.Background(), &api.Operation{DropAll: true})
-// }
-
-// func runTxn(c *dgo.Dgraph) {
-// 	txn := c.NewTxn()
-// 	defer txn.Discard()
-// }
-
-func init() {
-	// Joe = Person{
-	// 	Drugs: make(map[string]Drug),
-	// }
-
-	// TmpDrug = Drug{
-	// 	TargetReceptors: make(map[string]Receptor),
-	// }
-
-	// TmpReceptor = Receptor{
-	// 	SideEffects: make(map[string]SideEffect),
-	// }
-
-	// TmpSideEffect.Description = "death"
-	// TmpSideEffect.Count = 1
-
-	// TmpReceptor["sideEffect1"] := TmpSideEffect
-
-	// Joe["drug0"] = Drug0
-
-	// Joe.Drugs = []Drug{
-	// 	Drug{
-	// 		TargetReceptors: []Receptor{
-	// 			Receptor{
-	// 				SideEffects: []SideEffect{
-	// 					SideEffect{
-	// 						Description: "boom",
-	// 						Count:       1,
-	// 					},
-	// 					SideEffect{
-	// 						Description: "boom",
-	// 						Count:       1,
-	// 					},
-	// 				},
-	// 			},
-	// 			Receptor{
-	// 				SideEffects: []SideEffect{
-	// 					SideEffect{
-	// 						Description: "boom",
-	// 						Count:       1,
-	// 					},
-	// 					SideEffect{
-	// 						Description: "boom",
-	// 						Count:       1,
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// }
-}
-
-// OpenXMLFile Opens and XML File
-func OpenXMLFile(FileName string) (xmlFile *os.File) {
-	// Open our xmlFile
-	xmlFile, err := os.Open(FileName)
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("Successfully Opened %s\n", FileName)
-	// defer the closing of our xmlFile so that we can parse it later on
-	defer xmlFile.Close()
-
-	return xmlFile
-}
-
-// GetXMLFileName gets the name of the xml file we want to read from
-func GetXMLFileName() string {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
-
-	// fmt.Println("Enter text: ")
-	// text2 := ""
-	// fmt.Scanln(text2)
-	// fmt.Println(text2)
-
-	// ln := ""
-	// fmt.Sscanln("%v", ln)
-	// fmt.Println(ln)
-
-	return text
-}
+import "github.com/Noah-Heil/phren/cmd"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("What XML file would you like to read from?")
-	FileName := GetXMLFileName()
-	OpenXMLFile(FileName)
-
+	cmd.Execute()
 }
